ba06c79549cb8f30c2b3aaf15c8f56d42436b052
fabiano-debugador <fabiano_the_beast@hotmail.com>
2021-11-28 09:35:55 -0300

    first commit

---
+++ src/controllers/UserController.js
@@ -0,0 +1,55 @@
+const { update } = require('lodash')
+const { nextTick } = require('process')
+const { create } = require('yallist')
+const knex = require('../database')
+
+module.exports = {
+    async index(req, res) {
+        const results = await knex('users')
+        return res.json(results)
+    },
+
+    async create(req, res, next) {
+        try {
+            const { username } = req.body
+
+            const user = await knex('users').insert({
+                username
+            })
+
+            res.status(201).send()
+
+        } catch (error) {
+            next(error)
+        }
+    },
+
+    async update(req, res, next) {
+        try {
+            const { username } = req.body
+            const { id } = req.params
+
+            await knex('users')
+            .update({ username })
+            .where( { id })
+
+            return res.send()
+        } catch (error) {
+            next(error)
+        }
+    },
+
+    async delete(req, res, next) {
+        try {
+            const { id } = req.params
+
+            await knex('users')
+            .where({ id })
+            .del()
+
+            return res.status(204).send()
+        } catch (error) {
+            next(error)
+        }
+    }
+}

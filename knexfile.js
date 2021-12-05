3ac5e35cafa75e83bfd6b5601d6a780dee3e8bb3
fabiano-debugador <fabiano_the_beast@hotmail.com>
2021-12-05 18:26:57 -0300

    fix

--- knexfile.js
+++ knexfile.js
@@ -17,18 +17,18 @@ module.exports = {
       directory: `${__dirname}/src/database/seeds`
     }
   },
-    production: { 
-      client: 'pg', 
-      connection: process.env.DATABASE_URL,
-      ssl: { rejectUnauthorized: false },
-      migrations: {
-        tableName: 'knex_migrations',
-        directory: `${__dirname}/src/database/migrations`
-      },
-      seeds: {
-        directory: `${__dirname}/src/database/seeds`
-      } 
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: `${__dirname}/src/database/migrations`
     },
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`
+    }
+  },
   onUpdateTrigger: table => `
   CREATE TRIGGER ${table}_updated_at
   BEFORE UPDATE ON ${table}
